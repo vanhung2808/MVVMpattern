7d0e202b5c3685fa5738ec64d4b86b2e34d95da7
Hung Nguyen <hungnguyencntt2808@gmail.com>
2021-04-22 09:03:30 +0700

    add README

--- MVVMpattern/app/src/main/java/com/example/mvvmpattern/view/ActivitySignup.java
+++ MVVMpattern/app/src/main/java/com/example/mvvmpattern/view/ActivitySignup.java
@@ -3,17 +3,13 @@ package com.example.mvvmpattern.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mvvmpattern.R;
-import com.example.mvvmpattern.model.User;
 import com.example.mvvmpattern.viewmodel.UserViewModel;
 import com.example.mvvmpattern.viewmodel.UserViewModelFactory;
 
@@ -39,12 +35,14 @@ public class ActivitySignin extends AppCompatActivity {
                 String password = etPassword.getText().toString();
                 String configPassword = etConfigPassword.getText().toString();
 
-                if (userViewModel.findByUserName(email)) {
+                if (userViewModel.findExistsEmail(email)) {
                     Toast.makeText(getApplication(), "Đã tồn tại tài khoản", Toast.LENGTH_LONG).show();
                 }
-                else if (!userViewModel.findByUserName(email) && (password.equals(configPassword)) && (email != null)) {
-                    User user = new User(email,password);
-                    userViewModel.insertUser(user);
+                if (email == null) {
+                    Toast.makeText(getApplication(),"Thông tin đăng kí không hợp lệ", Toast.LENGTH_LONG).show();
+                }
+                else if (!userViewModel.findExistsEmail(email) && (password.equals(configPassword)) && (email != null) && (password.length() >= 6)) {
+                    userViewModel.insertUser(userViewModel.newUser(email,password));
                     Toast.makeText(getApplication(),"Đã đăng kí thành công", Toast.LENGTH_LONG).show();
                 }
                 else {
